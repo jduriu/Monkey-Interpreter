12734fa9aabc91c7dad2f169908235f5f5fb8097
jduriu <jduriu@gmail.com>
2024-07-25 10:52:39 -0700

    setup initial parser

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -18,33 +18,31 @@ type Expression interface {
 	expressionNode()
 }
 
-type Program interface {
+type Program struct {
 	Statements []Statement
 }
-
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
-		} else {
-			return ""
-		}
+	} else {
+		return ""
 	}
+}
 
 type LetStatement struct {
-	Token token.Token  // the token.LET token
-	name *Identifier
+	Token token.Token // the token.LET token
+	name  *Identifier
 	value Expression
 }
 
-
-func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 type Identifier struct {
-	Token token.Token  // the token.IDENT token
+	Token token.Token // the token.IDENT token
 	Value string
 }
 
-func (i *Identifier) expressionNode() {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
